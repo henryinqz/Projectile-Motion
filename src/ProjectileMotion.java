4340343427da9a95e82479f5bbc56693965f435e
Henry <36207470+henryinqz@users.noreply.github.com>
2020-02-29 12:11:22 -0500

    Cleaning up directories

--- src/ProjectileMotion.java
+++ src/ProjectileMotion.java
@@ -17,6 +17,7 @@ public class ProjectileMotion {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); // Modern Look&Feel UI
+            //UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName()); // Fixes Mac devices showing native JComponent styles
         } catch (Exception e) {
             e.printStackTrace(); // Print where error is
         }
